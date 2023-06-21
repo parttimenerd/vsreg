623c08f4b592eeedd582935ace088816e19857cd
Johannes Bechberger <johannes.bechberger@sap.com>
2023-06-21 13:17:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,108 @@
+vsreg
+=====
+
+Debugging JTREG tests with VSCode is difficult, you have to write the launch.json file by hand.
+But worry no more: This small utility will do this for you :)
+
+Just pass the make test command to it (with `JTREG="VERBOSE=all"`) with a test label,
+and vsreg updates the `launch.json` file for you:
+
+```sh
+git clone https://github.com/parttimenerd/vsreg
+vsreg/vsreg.py "ASGCT" -- make test TEST=jtreg:test/hotspot/jtreg/serviceability/AsyncGetCallTrace JTREG="VERBOSE=all"
+```
+
+... and you can start debugging with VSCode, recompile your tests with `make test-image`.
+You can add a task to your `tasks.json` file and passe the label to the ``:
+
+```json
+{
+  "label": "Make test-image",
+  "type": "shell",
+  "options": {
+      "cwd": "${workspaceFolder}"
+  },
+  "command": "/usr/bin/gmake",
+  "args": ["test-image"],
+  "problemMatcher": ["$gcc"]
+}
+```
+
+To try vsreg without modifying your `launch.json` file, you can use the `--dry-run` option:
+
+```sh
+./vsreg.py "ASGCT" --dry-run -- make test TEST=jtreg:test/hotspot/jtreg/serviceability/AsyncGetCallTrace JTREG="VERBOSE=all"
+```
+
+The tool fills the passed template (default `gdb`) which can be configured with the `--template` option.
+The default template looks like this:
+
+```json
+{
+  "name": "$NAME",
+  "type": "cppdbg",
+  "request": "launch",
+  "program": "",
+  "args": [],
+  "stopAtEntry": false,
+  "cwd": "",
+  "environment": [],
+  "externalConsole": false,
+  "MIMode": "gdb",
+  "miDebuggerPath": "/usr/bin/gdb",
+  "subProcess": true,
+  "setupCommands": [
+    {
+      "description": "Enable pretty-printing for gdb",
+      "text": "-enable-pretty-printing",
+      "ignoreFailures": true
+    },
+    {   "description": "The new process is debugged after a fork. The parent process runs unimpeded.",
+        "text": "-gdb-set follow-fork-mode child",
+        "ignoreFailures": true
+    }
+  ],
+  "preLaunchTask": ""
+}
+```
+
+Please be aware that only single tests are supported, not test suites.
+
+Options
+-------
+```shell
+usage: vsreg.py [-h] [-t TEMPLATE] [-d] [-b TASK] LABEL COMMAND [COMMAND ...]
+
+Create a debug launch config for a JTREG test run
+
+positional arguments:
+  LABEL                 Label of the config
+  COMMAND               Command to run
+
+options:
+  -h, --help            show this help message and exit
+  -t TEMPLATE, --template TEMPLATE
+                        Template to use for the launch config, or name of file
+                        without suffix in vsreg/template folder
+  -d, --dry-run         Only print the launch config
+  -b TASK, --build-task TASK
+                        Task to run before the command
+```
+
+*The tool is inspired by [bear](https://github.com/rizsotto/Bear)*
+
+Requirements
+------------
+- Python 3.10 or newer
+- Linux (macOS support is coming)
+
+Contributing
+------------
+I'm happy for any contributions, like new templates, just open an issue or a pull request :)
+
+
+License
+-------
+MIT, Copyright 2023 SAP SE or an SAP affiliate company, Johannes Bechberger and vsreg contributors
+
+This project is a prototype of the [SapMachine](https://sapmachine.io) team at [SAP SE](https://sap.com).
---
+++ vsreg.py
@@ -0,0 +1,173 @@
+#! /usr/bin/python3
+
+import argparse
+import json
+import os
+import re
+import shlex
+import subprocess
+import sys
+from dataclasses import dataclass
+from pathlib import Path
+from pprint import pprint
+from typing import Any, Dict, List, Optional
+
+VSREG_FOLDER = Path(__file__).parent
+CUR_FOLDER = Path.cwd()
+VSCODE_FOLDER = CUR_FOLDER / ".vscode"
+
+
+@dataclass
+class LaunchConfig:
+    data: Dict[str, Any]
+
+    def name(self) -> str:
+        return self.data["name"]
+
+
+class LaunchConfigs:
+
+    def __init__(self, data: Dict[str, Any]):
+        self.data = data
+
+    def write(self, path: Path):
+        with path.open("w") as f:
+            json.dump(self.data, f, indent=2)
+
+    @staticmethod
+    def read(path: Path) -> 'LaunchConfigs':
+        with path.open("r") as f:
+            return LaunchConfigs(json.loads(f.read()))
+
+    @staticmethod
+    def empty() -> 'LaunchConfigs':
+        return LaunchConfigs({"version": "0.2.0", "configurations": []})
+
+    def __contains__(self, config: LaunchConfig) -> bool:
+        return any(c["name"] == config.name() for c in self.data["configurations"])
+
+    # replaces if exists
+    def add(self, config: LaunchConfig):
+        if config not in self:
+            self.data["configurations"].append(config.data)
+        else:
+            self.data["configurations"] = [config.data if c["name"] == config.name() else c for c in
+                                           self.data["configurations"]]
+
+
+@dataclass
+class Parsed:
+    cwd: str
+    env: Dict[str, str]
+    program: str
+    args: List[str]
+
+
+def run(label: str, launch_template: Path, task_template: Path, command: str, dry_run: bool, build_task: Optional[str]):
+    pass
+
+
+@dataclass
+class CommandResult:
+    stdout: str
+    env: Dict[str, str]  # additional env vars
+
+
+def run_command(command: List[str]) -> CommandResult:
+    assert "make" in command, "make not found in command"
+    env_vars = {parts[0]:parts[1] for env in command[0:command.index("make")] if len(parts := env.split("=", 2)) == 2}
+    environ = dict(os.environ)
+    environ.update(env_vars)
+    out = subprocess.run(shlex.join(command), shell=True, env=environ, capture_output=True).stdout.decode("utf-8")
+
+    return CommandResult(out, env_vars)
+
+def parse(command_out: CommandResult) -> Parsed:
+    parts = command_out.stdout.split("rerun:\ncd")
+    assert len(parts) >= 2, "Unexpected output format"
+    parts = parts[-1].split("\n\n")
+    assert len(parts) >= 2, "Unexpected output format"
+    lines = parts[0].split("\n")
+    assert lines[0].startswith(" /") and lines[0].endswith(" && \\"), "Unexpected output format: " + lines[0]
+    cwd = lines[0][1:-5]
+    assert Path(cwd).exists(), f"Invalid cwd: {cwd}"
+    lines = lines[1:]
+    env_lines_length = next(i for i, line in enumerate(lines) if line.startswith(" "))
+    env_lines = lines[:env_lines_length]
+    env: Dict[str, str] = {}
+    for line in env_lines:
+        line = line[:-2]
+        assert re.match(r"^[A-Z_]+=.+$", line), f"Unexpected env line: {line}"
+        key, value = line.split("=", 1)
+        env[key] = value
+    java_lines = lines[env_lines_length:]
+    java = java_lines[0].strip()[:-2]
+    assert java.endswith("java"), f"Unexpected java command: {java}"
+    assert Path(java).exists(), f"Invalid java command: {java}"
+    java_args = java_lines[1:]
+    args: List[str] = []
+    for arg in java_args[:-1]:
+        arg = arg.strip()
+        assert arg.endswith(" \\"), f"Unexpected java arg: {arg}"
+        args.append(arg[:-2])
+    args.extend(shlex.split(java_args[-1].strip()))
+
+    return Parsed(cwd, {**env, **command_out.env}, java, args)
+
+
+def load_template(template: str) -> Dict[str, Any]:
+    file = Path(template if template.endswith(".json") else VSREG_FOLDER / "templates" / (template + ".json"))
+    assert file.exists(), f"Template file does not exist: {file}"
+    return json.loads(file.read_text())
+
+
+def create_launch_config(label: str, parsed: Parsed, template: str, build_task: Optional[str]) -> LaunchConfig:
+    template_json = load_template(template)
+    if "$NAME" in template_json["name"]:
+        template_json["name"] = template_json["name"].replace("$NAME", label)
+    else:
+        template_json["name"] = label
+    template_json["cwd"] = parsed.cwd
+    template_json["environment"] = [{"name": name, "value": value} for name, value in
+                                    sorted(parsed.env.items(), key=lambda x: x[0])]
+    template_json["program"] = parsed.program
+    template_json["args"] = ["-XX:+UnlockDiagnosticVMOptions", "-XX:+WhiteBoxAPI"] + parsed.args
+    if build_task:
+        template_json["preLaunchTask"] = build_task
+    return LaunchConfig(template_json)
+
+
+if __name__ == '__main__':
+    # source https://groups.google.com/g/argparse-users/c/LazV_tEQvQw/m/xJhBOm1qS5IJ
+    class MyParser(argparse.ArgumentParser):
+        def error(self, message):
+            sys.stderr.write('error: %s\n' % message)
+            self.print_help()
+            sys.exit(2)
+
+
+    parser = MyParser(description='Create a debug launch config for a JTREG test run')
+    parser.add_argument('label', metavar='LABEL', type=str, help='Label of the config')
+    parser.add_argument('-t', '--template', metavar='TEMPLATE', type=str,
+                        help='Template to use for the launch config, or name of file without suffix in vsreg/template folder',
+                        required=False, default='gdb')
+    parser.add_argument('-d', '--dry-run', action='store_true', help='Only print the launch config', required=False)
+    parser.add_argument('-b', '--build-task', metavar='TASK', type=str, help='Task to run before the command',
+                        required=False)
+    parser.add_argument('command', metavar='COMMAND', type=str, nargs='+', help='Command to run')
+    args = parser.parse_args()
+    parsed = parse(run_command(args.command))
+    launch_config = create_launch_config(args.label, parsed, args.template, args.build_task)
+    if args.dry_run:
+        print(json.dumps(launch_config.data, indent=2))
+    else:
+        if not VSCODE_FOLDER.exists():
+            VSCODE_FOLDER.mkdir(parents=True)
+        file = VSCODE_FOLDER / "launch.json"
+        launch = LaunchConfigs.read(file) if file.exists() else LaunchConfigs.empty()
+        if launch_config in launch:
+            print(f"Replacing launch config {launch_config.name()}")
+        else:
+            print(f"Adding launch config {launch_config.name()}")
+        launch.add(launch_config)
+        launch.write(file)
